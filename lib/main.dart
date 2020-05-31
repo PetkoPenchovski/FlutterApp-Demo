d1d134716184e9160e18a44bd058719c839ab6cc
LAPTOP-ACER <petkopenchovski@gmail.com>
2020-05-31 17:47:06 -0400

    add lucky numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:flutterapp/app_screens/firs_screen.dart';
 
-void main(){
-  //Inflate the widget and attached to the screen
-  runApp(
-      Center(
-          child: Text
-        (
-        "Hello Flutter", textDirection: TextDirection.ltr,
-          )
-      )
-  );
+// Entry point of the App.Inflate the widget and attached to the screen
+void main() => runApp(new MyFlutterApp());
+
+//subclass            superclass
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My Flutter App",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App Screen"),
+          ),
+          body: FirstScreen()),
+    );
+  }
 }
