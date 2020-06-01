a9284a66ce5c5946ec215c2153796575c33af8ff
LAPTOP-ACER <petkopenchovski@gmail.com>
2020-06-01 18:23:50 -0400

    create onSubmit in SteteFul

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,53 @@
 import 'package:flutter/material.dart';
-import 'app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Long List"),
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          debugPrint("FAB clicked");
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add One More Item',
-      ),
-    ),
+    title: "Stateful App Example",
+    home: FavoriteCity(),
   ));
 }
 
-//Widget getListView() {
-//  var listView = ListView(
-//    children: <Widget>[
-//
-//      ListTile(
-//        leading: Icon(Icons.landscape),
-//        title: Text("Landscape"),
-//        subtitle: Text("Beautiful View!"),
-//        trailing: Icon(Icons.wb_sunny),
-//        onTap: (){
-//          debugPrint("Landscape tapped");
-//        },
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.laptop_chromebook),
-//        title: Text("Windows"),
-//        onTap: (){
-//          debugPrint("Landscape tapped");
-//        },
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text("Phone"),
-//        onTap: (){
-//          debugPrint("Landscape tapped");
-//        },
-//      ),
-//
-//      Text("Yet another element in List"),
-//
-//      Container(color: Colors.red, height: 50.0,)
-//    ],
-//  );
-//  return listView;
-//}
-
-void showSnackBar(BuildContext context, String item) {
-  var snackBar = SnackBar(
-    content: Text("You just tapped $item"),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: (){
-        debugPrint('Performing dummy UNDO operation');
-      },
-    ),
-  );
-
-  Scaffold.of(context).showSnackBar(snackBar);
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
 }
 
-List<String> getListElements() {
-  var items = List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
-}
+class _FavoriteCityState extends State<FavoriteCity> {
+  String nameCity = "";
 
-Widget getListView() {
-  var listItems = getListElements();
+  @override
+  Widget build(BuildContext context) {
 
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    return ListTile(
-      leading: Icon(Icons.arrow_right),
-      title: Text(listItems[index]),
-      onTap: () {
-        showSnackBar(context, listItems[index]);
-      },
+    debugPrint("Favorite City widget is creating");
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful App Example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  debugPrint("Set State is called, this tell framework redraw the FavCity widget");
+                  nameCity = userInput;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Your best city is $nameCity",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
     );
-  });
-
-  return listView;
+  }
 }
