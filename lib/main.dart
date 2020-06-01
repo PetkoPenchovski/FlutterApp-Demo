e723f05dbc7a679315a21914378a9039749a79e2
LAPTOP-ACER <petkopenchovski@gmail.com>
2020-06-01 10:51:15 -0400

    adding columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import "package:flutter/material.dart";
-import 'package:flutterapp/app_screens/firs_screen.dart';
+import 'package:flutter/material.dart';
+import 'app_screens/home.dart';
 
-// Entry point of the App.Inflate the widget and attached to the screen
-void main() => runApp(new MyFlutterApp());
-
-//subclass            superclass
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Flutter App",
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("My First App Screen"),
-          ),
-          body: FirstScreen()),
-    );
-  }
+void main(){
+  runApp(MaterialApp(
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
