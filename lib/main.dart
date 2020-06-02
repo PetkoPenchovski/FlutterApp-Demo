8f1bd1f6ce033ad24cf7ada4992a55e6446657fd
LAPTOP-ACER <petkopenchovski@gmail.com>
2020-06-01 20:00:49 -0400

    add all UI elements of the calculator

--- lib/main.dart
+++ lib/main.dart
@@ -2,62 +2,114 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Stateful App Example",
-    home: FavoriteCity(),
+    title: "Simple Interest Calculator App",
+    home: SIForm(),
   ));
 }
 
-class FavoriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _FavoriteCityState();
+    return _SIFormState();
   }
 }
 
-class _FavoriteCityState extends State<FavoriteCity> {
+class _SIFormState extends State<SIForm> {
   String nameCity = "";
-  var _currencies = ['Lev', 'Dollar', 'Euro', 'Kuna'];
-  var _ccurentItemSelected = 'Lev';
+  final _minimumMargin = 5.0;
+  final _minimumPadding = 5.0;
+
+  var _currencies = ['Dollar', 'Lev', 'Euro', 'Kuna'];
+  var _ccurentItemSelected = 'Dollar';
 
   @override
   Widget build(BuildContext context) {
     debugPrint("Favorite City widget is creating");
 
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text("Stateful App Example"),
+        title: Text("Simple Interest Calculator App"),
       ),
       body: Container(
-        margin: EdgeInsets.all(20.0),
-        child: Column(
+        margin: EdgeInsets.all(_minimumMargin * 2),
+        child: ListView(           //ListView allow the App Screen to be adaptive to different screen devices
           children: <Widget>[
-            TextField(
-              onSubmitted: (String userInput) {
-                setState(() {
-                  debugPrint(
-                      "Set State is called, this tell framework redraw the FavCity widget");
-                  nameCity = userInput;
-                });
-              },
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String dropDownStringItem) {
-                return DropdownMenuItem<String>(
-                  value: dropDownStringItem,
-                  child: Text(dropDownStringItem),
-                );
-              }).toList(),
-              onChanged: (String newValueSelected) {
-                _onDropDownItemSelected(newValueSelected);
-              },
-              value: _ccurentItemSelected,
-            ),
+            getImageAsset(),
             Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text(
-                "Your best city is $nameCity",
-                style: TextStyle(fontSize: 20.0),
-              ),
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Principal',
+                      hintText: 'Enter Principal e.g. 1000',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Rate of Interest',
+                      hintText: 'In percent %',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              labelText: 'Rate of Interest',
+                              hintText: 'In percent %',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0)))),
+                    ),
+                    Container(
+                      width: _minimumPadding * 5,
+                    ),
+                    Expanded(
+                        child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: 'Dollar',
+                      onChanged: (String newValueSelected) {},
+                    ))
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Calculate'),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Reset'),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                )),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding * 2),
+              child: Text('TODO Next'),
             )
           ],
         ),
@@ -65,9 +117,12 @@ class _FavoriteCityState extends State<FavoriteCity> {
     );
   }
 
-  void _onDropDownItemSelected(String newValueSelected) {
-    setState(() {
-      this._ccurentItemSelected = newValueSelected;
-    });
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/bank.png');
+    Image image = Image(image: assetImage, width: 125.0, height: 125.0);
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumMargin * 10),
+    );
   }
 }
